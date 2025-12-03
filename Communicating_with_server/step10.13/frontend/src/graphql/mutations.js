0dfa96c62f5fd1d66a85746bb5054206c59ce616
Nandini-Mane <nandinimane2005@gmail.com>
2025-12-03 11:33:36 +0530

    update

--- Communicating_with_server/step10.13/frontend/src/graphql/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const AUTHENTICATE = gql`
-  mutation Authenticate($username: String!, $password: String!) {
-    authenticate(credentials: { username: $username, password: $password }) {
-      accessToken
-    }
-  }
-`;
--- Testing-and-extending-our-application/step10.19/src/apollo/client.js
+++
@@ -1,9 +0,0 @@
-// src/apollo/client.js
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://your-graphql-server.com/graphql', // replace with your GraphQL server
-  cache: new InMemoryCache(),
-});
-
-export default client;
